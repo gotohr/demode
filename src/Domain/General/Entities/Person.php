2a6cca3a2be050690e038c71785e90220fdc36f0
Ljubo Čanić <ljubo@goto.hr>
2014-03-15 15:06:17 +0100

    moved some classes

--- src/Domain/General/Entities/Person.php
+++ src/Domain/General/Entities/Person.php
@@ -1,22 +1,23 @@
 <?php
 
-namespace Domain\General;
+namespace Domain\General\Entities;
 
+use Domain\General\string;
 use Modeling\Entity;
 use Modeling\Property;
 
 /**
- * @method \Domain\General\Person setFirstName($firstName)
+ * @method \Domain\General\Entities\Person setFirstName($firstName)
  * @method string getFirstName()
- * @method \Domain\General\Person setLastName($lastName)
+ * @method \Domain\General\Entities\Person setLastName($lastName)
  * @method string getLastName()
  */
 class Person extends Entity {
 
     protected function _build() {
         $this->getProperties()
-            ->addProperty(Property::create('firstName'))
-            ->addProperty(Property::create('lastName'))
+            ->add(Property::create('firstName'))
+            ->add(Property::create('lastName'))
         ;
     }
 
--- tests/ModelingTest.php
+++ tests/ModelingTest.php
@@ -1,6 +1,6 @@
 <?php
 
-class PersonTest extends PHPUnit_Framework_TestCase {
+class ModelingTest extends PHPUnit_Framework_TestCase {
 
     public function testPersonCreation() {
         $person = new \Domain\General\Person();
