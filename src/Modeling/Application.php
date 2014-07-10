c2fd0edf83d88b2c966f6217509066b3128e349b
Ljubo Čanić <ljubo.canic@aduro.hr>
2014-07-10 17:21:41 +0200

    and more of that...

--- src/Modeling/Application.php
+++ src/Modeling/Application.php
@@ -17,4 +17,22 @@ class Application {
     public function has() {
         return $this;
     }
+
+    /**
+     * @return Application
+     */
+    public function show() {
+        return $this;
+    }
+
+    /**
+     * @return Application
+     */
+    public function build() {
+        return $this;
+    }
+
+    public function describe() {
+        return "brand new app! :) ";
+    }
 } 
--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -7,19 +7,32 @@ use Modeling\View;
 class ApplicationTest extends PHPUnit_Framework_TestCase {
 
     public function testApplicationCreation() {
-        Application::create()
+        $app = Application::create()
             ->has(
-                Display::create()
+                $dashboard = Display::create(),
+                $login = Display::create()
                     ->layout('center')
                     ->show(
                         View::create()
                             ->from(\Domain\General\Model\User::create())
                             ->asDataEntry()
                     )
-                    ->handlePost()
+                    ->handlePost(
+                        function($data) use (&$app, $dashboard) {
+                            /** @var Application $app */
+                            /** @var \Domain\General\Model\User $data */
+                            $isAuthenticated = $app->getService('authenticate', $data);
+                            if ($isAuthenticated) {
+                                $app->show($dashboard);
+                            }
+                        }
+                    )
             )
+            ->show($login)
+            ->build();
         ;
 
+        echo $app->describe();
         // app displays login screen where we can enter login data (username and password)
         // login screen verifies data against service
     }
